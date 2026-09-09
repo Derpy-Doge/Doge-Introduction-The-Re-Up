4a4ae7cb8c43e8c1955e4357d45d9301c08e6b59
MUKILTEO\1725577 <kbw2205@icloud.com>
2026-09-09 11:15:35 -0700

    gerbil

---
+++ Assets/Scripts/Bingus.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Bingus : MonoBehaviour
+{
+    public float moveSpeed = 10f;
+
+    public Rigidbody2D rb2d;
+    public 
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        
+    }
+}
