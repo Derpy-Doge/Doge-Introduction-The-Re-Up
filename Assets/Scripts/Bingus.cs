96881a0973e085025b94b5379b5af61faa2f0501
MUKILTEO\1725577 <kbw2205@icloud.com>
2026-09-09 13:35:38 -0700

    cassowary

--- Assets/Scripts/Bingus.cs
+++ Assets/Scripts/Bingus.cs
@@ -1,19 +1,33 @@
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Bingus : MonoBehaviour
 {
-    public float moveSpeed = 10f;
-
+    public float moveSpeed = 5f;
+    public float jumpForce = 5f;
     public Rigidbody2D rb2d;
-    public 
+    public Animator animator;
 
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
-    void Update()
+    void FixedUpdate()
     {
-        
+        rb2d.linearVelocityX = moveSpeed;
+    }
+
+    public void Phoenix(InputAction.CallbackContext ctx)
+    {
+        if (ctx.ReadValue<float>() == 0)
+        {
+            return;
+        }
+
+        rb2d.linearVelocityY = jumpForce;
+        animator.SetTrigger("john");
     }
 }
